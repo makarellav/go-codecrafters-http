ad17282741441fcfbc7aba51dd62c81df5540521
makarellav <makarellads@gmail.com>
2024-03-14 10:05:19 +0200

    pass 2nd stage

--- app/server.go
+++ app/server.go
@@ -36,7 +36,7 @@ func main() {
 		os.Exit(1)
 	}
 
-	_, err = conn.Write([]byte(`HTTP/1.1 200 OK\r\n\r\n`))
+	_, err = conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
 
 	if err != nil {
 		fmt.Println("Failed to write data", err.Error())
